fd4ece604ac63dfe25f7cec45793f4d23775cef0
dumppople <2253318913@qq.com>
2023-11-24 20:16:27 +0800

    1223

---
+++ src/main/java/net/yuanqi/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.yuanqi.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
